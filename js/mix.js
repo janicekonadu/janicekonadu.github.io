27f5d5ddcd4570f1a3115e48151a725b0538d58a
jkonadu777 <jkonadu777@gmail.com>
2016-05-17 16:19:07 -0400

    modifications to mix app and extra cleaning up

--- js/mix.js
+++ js/mix.js
@@ -1,60 +1,166 @@
 
+(function() {
+
+  /* Create Audio Elements:
+
+      > audio: creates <audio/> element in page (Your music artist i.e. Lil' Wayne)
+
+      > source: takes input from the audio (Think of a microphone.)
+      > analyser: what manipulates the music (think of the autotune 
+        that manipulates Lil' Wayne's voice)
+      > audioContext: the output destination (think of an amp that projects the
+        manipulated version of his voice)      
+         - AudioContext: specifies audio context for designated browser (Chrome)
+
+  */
+
+  var AudioContext;
+  var audio;
+  var audioContext;
+  var source;
+  var analyser;
+
+  /*
+      > canvas: gets canvas element from HTML
+      > canvasContext: let's create things 2d 
+  */
+
+  var canvas = document.getElementById("theCanvas");
+  var canvasContext = canvas.getContext("2d");    
+
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
+  
+
+  /*Music data variables*/
+
+  var freqByteData;
+  var analyserMethod = "getByteTimeDomainData";
+  var streamUrl;
 
 
-$( document ).ready(function() {
+  /* 
+      > Connect everything: 
+        source to analyser, analyser to ctx (or ctx destination in this case)  
+        
+        audio -> source -> analyser -> ctx
+        Lil' Wayne's voice -> Microphone-> Autotune -> Speaker
 
-    
-    SC.initialize({
-      client_id: '587a61d352df592938176dce1406b818'
-    });
+  */
+
+  function initAudio(streamUrl) {
+
+    AudioContext = window.AudioContext || window.webkitAudioContext;
+    audio = new Audio();
+    audio.crossOrigin = "anonymous"; // handle cross-domain restriction
+    audio.controls = "true";
+    audioContext = new AudioContext();
+    source = audioContext.createMediaElementSource(audio);
+    analyser = audioContext.createAnalyser();            
+    source.connect(analyser);    
+    analyser.connect(audioContext.destination);
+
+  };
+
+  function get(url, callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() { 
+      if (request.readyState === 4 && request.status === 200) {
+        callback(request.responseText);
+      }
+    }
+
+    request.open("GET", url, true);            
+    request.send(null);
+  }
+
+  var clientID = "client_id=587a61d352df592938176dce1406b818"
+  var trackPermalinkUrl = "http://soundcloud.com/rosemary-fairweather/too-low";
+
+  // Resove method promises JSON representation of official soundlcloud link with proper authentication
+
+  function findTrack() {
+    get("http://api.soundcloud.com/resolve.json?url=" +  trackPermalinkUrl + "&" + clientID,
+        function (response) {
+      var trackInfo = JSON.parse(response);
+      streamUrl = trackInfo.stream_url + "?" + clientID;
+    }
+       );
+  };
 
 
-    // stream track id 202409210
-    SC.stream('/tracks/202409210').then(function(sound){
+  function createBarAnimation() {
+ 
+    analyser.fftSize = 2048;
+    var bufferLength = 100;
 
-          var ispaused = true;
+    freqByteData = new Uint8Array(bufferLength); // creates an empty array for music data with 100 spaces
+    canvasContext.fillStyle = "#aaa28a"; // color of the bars
 
-          $('.spinner-wrap').click(function(e) {
+    function drawAgain() {
+      canvasContext.clearRect(0, 0, canvasWidth, canvasHeight); // clear the canvas
+      requestAnimationFrame(drawAgain); // looping
 
-              e.preventDefault();
+      analyser[analyserMethod](freqByteData); // fill array with values from analyser
 
-              var $this = $(this),
+      for(var i = 0; i < bufferLength; i++){
+          bar_x = i * 20;
+          bar_width = 5;
+          bar_height = -(freqByteData[i*2]/2); // initialize heights via values in freqByteData
 
-                bpm = Number($this.siblings('audio').data('bpm'))
-                pulse = (60/bpm)*1000;
-              
-              // if song is playing
-              if (ispaused === false) {
-                sound.pause();
-                sound.seek(0);
-                $this.removeClass('playing');
-                clearInterval(intervals);
-                ispaused = true;
-              }
-              
-              // if song is paused 
-              else {
-                sound.play();
-                $this.addClass('playing');
-                pulsing();
-                intervals = setInterval(function() {pulsing()}, pulse);
-                ispaused = false;
-                
-              }
+          //  fillRect( x, y, width, height ) 
+          canvasContext.fillRect(bar_x, canvas.height, bar_width, bar_height);
+      }
+    }
+
+    drawAgain(); 
+  }
+
+  var ispaused = false;
+
+  // Click function for custom play/pause button
+
+  $('#playButton').click(function() {
+
+          var self = $(this);
+
+          if (ispaused == false){
+              audio.src = streamUrl;
+              audio.looping = "looping";
+              audio.play();
+
+              createBarAnimation();
+              self.addClass('playing');
+
+              var bpm = 42.46,
+              pulse = (60/bpm)*1000;
 
               function pulsing() {
                 
-                $this.addClass('pulse');
+                self.addClass('pulse');
                 
                 setTimeout(function() {
-                  $this.removeClass('pulse');  
+                  self.removeClass('pulse');
                 }, pulse-100);
                 
-              }
+              } 
 
-          });
-    
-    
+              pulsing();
+              intervals = setInterval(function() {pulsing()}, pulse);
+              ispaused = true;
+            }
+
+       else {
+                audio.pause();
+                self.removeClass('playing');
+                clearInterval(intervals);
+                ispaused = false;
+            }
+            
+
   });
 
-});
+
+  findTrack();
+  initAudio();
+})();
